307dd1430b3df04a70da189db36870501f1437ce
Kizaru74 <stark.kizaru74@gmail.com>
2025-11-20 02:38:15 -0600

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // CRÍTICO para GitHub Pages: Usa el nombre de tu repositorio
+  base: '/nombre-de-tu-repositorio/', 
+  plugins: [],
+});
