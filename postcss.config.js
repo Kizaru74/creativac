b438f101e1a62f0710936415f32b2c785c713231
Kizaru74 <stark.kizaru74@gmail.com>
2025-11-20 01:54:54 -0600

    Update PostCSS configuration to remove plugins
    Removed Tailwind CSS and Autoprefixer plugins from configuration.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-  }
+  content: [
+    "./index.html", // Escanea el HTML
+    "./main.js",    // Escanea el JS donde inyectas clases dinámicas
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
